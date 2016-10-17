baceab507a2e697f0fddeda9e0b78bde176acbf5
s-voloshynenko <s.voloshynenko@gmail.com>
2016-10-17 18:30:55 +0300

    compile i18n

--- src/main_i18n-native.ts
+++ src/main_i18n-native.ts
@@ -1,6 +1,8 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AppModule } from './app-i18nNative/app.module';
+import { TRANSLATION } from './app-i18nNative/locale/messages.fr';
 
 // depending on the env mode, enable prod mode or add debugging modules
 if (process.env.ENV === 'build') {
@@ -8,10 +10,20 @@ if (process.env.ENV === 'build') {
 }
 
 export function main() {
-  return platformBrowserDynamic().bootstrapModule(AppModule);
+  const locale = document['locale'] as string;
+
+  return platformBrowserDynamic().bootstrapModule(AppModule, {
+    providers: [
+      { provide: TRANSLATIONS, useValue: TRANSLATION },
+      { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+      { provide: LOCALE_ID, useValue: locale }
+    ]
+  });
 }
 
 if (document.readyState === 'complete') {
+  document['locale'] = 'fr';
+
   main();
 } else {
   document.addEventListener('DOMContentLoaded', main);
